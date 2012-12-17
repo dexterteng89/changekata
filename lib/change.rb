1bf9bd3df3a0fa23a023d92a7159e37c0aa5e7f8
dexterteng89 <dexterteng89@gmail.com>
2012-12-17 14:14:25 -0600

    Update lib/change.rb

--- lib/change.rb
+++ lib/change.rb
@@ -1,15 +1,15 @@
 class Change
   def make_change_for(dollar_amt)
-  	total_pennies = (dollar_amt * 100).to_i
-  	coins = [25, 10, 5, 1]
-  	array = []
-  	coins.each do |coin|
-  		num_of_coin = total_pennies / coin
-	  	num_of_coin.times do 
-	  		array << coin
-	  		total_pennies -= coin
-	  	end
-	  end
-		array
+    total_pennies = (dollar_amt * 100).to_i
+    coins = [25, 10, 5, 1]
+    array = []
+    coins.each do |coin|
+      num_of_coin = total_pennies / coin
+      num_of_coin.times do 
+        array << coin
+	total_pennies -= coin
+      end
+    end
+    array
   end
 end
