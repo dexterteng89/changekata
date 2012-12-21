aec80debbef8816353b3428e30d1c2a338cb1960
Brian Kung <draklyne@gmail.com>
2012-12-21 13:53:48 -0600

    Refactored, adds test

--- lib/change.rb
+++ lib/change.rb
@@ -1,10 +1,9 @@
 class Change
 
   def make_change_for(dollar_amt)
-    return nil if dollar_amt.is_a?(Numeric) == false
-    return nil if dollar_amt < 0
+    return nil unless dollar_amt.is_a?(Numeric) && dollar_amt >= 0
     total_pennies = (dollar_amt * 100).to_i
-    coins = [25, 10, 5, 1]
+    coins = [500, 25, 10, 5, 1]
     array = []    
     coins.each do |coin|
       num_of_coin = total_pennies / coin
